b3812bb1f74d1ebc880023551caaf67326d9d3d9
feermoya <fmoya97.fm@gmail.com>
2026-05-09 11:03:25 -0300

    Mejoras

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
  */
 const siteUrl =
   process.env.PUBLIC_SITE_URL?.replace(/\/$/, '') ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://fermoyadev.com.ar');
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://www.fermoyadev.com.ar');
 
 export default defineConfig({
   site: siteUrl,
